88f120e1003946e05f12a89671f76b5c5068d593
Nguyên Bổn <huynhnguyenbon@gmail.com>
2026-04-21 20:45:23 +0700

    Add files via upload

---
+++ backend/src/main/java/com/BBTT/BBTTResort/BbttResortApplication.java
@@ -0,0 +1,13 @@
+package com.BBTT.BBTTResort;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BbttResortApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BbttResortApplication.class, args);
+	}
+
+}
