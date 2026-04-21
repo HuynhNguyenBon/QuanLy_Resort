df2526d90e9748da1608973ec4d45c8d6068480f
Nguyên Bổn <huynhnguyenbon@gmail.com>
2026-04-21 20:41:17 +0700

    Test

---
+++ backend/src/test/java/com/BBTT/BBTTResort/BbttResortApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BBTT.BBTTResort;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BbttResortApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
