9eb0e17e07cde409202a52ca4a59d214f3b40afc
HuynhGiaBao <gia797978@gmail.com>
2026-04-23 09:29:37 +0700

    add IUserService

---
+++ backend/src/main/java/com/BBTT/BBTTResort/service/interfac/IUserService.java
@@ -0,0 +1,22 @@
+package com.BBTT.BBTTResort.service.interfac;
+
+import com.BBTT.BBTTResort.dto.LoginRequest;
+import com.BBTT.BBTTResort.dto.Response;
+import com.BBTT.BBTTResort.entity.User;
+
+public interface IUserService {
+    Response register(User user);
+
+    Response login(LoginRequest loginRequest);
+
+    Response getAllUsers();
+
+    Response getUserBookingHistory(String userId);
+
+    Response deleteUser(String userId);
+
+    Response getUserById(String userId);
+
+    Response getMyInfo(String email);
+
+}
