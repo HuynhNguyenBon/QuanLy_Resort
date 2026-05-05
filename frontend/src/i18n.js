8ab3ed14df5f6128ab2593beae48a8375d62b177
HungBuiHuu <hungbui12345h@gmail.com>
2026-05-05 13:32:02 +0700

    up lai

--- frontend/src/i18n.js
+++ frontend/src/i18n.js
@@ -3,7 +3,7 @@ import { initReactI18next } from "react-i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
 import en from "./i18n/locales/en.json";
 import vi from "./i18n/locales/vi.json";
-import ja from "./i18n/locales/jp.json";
+import jp from "./i18n/locales/jp.json";
 
 i18n
   .use(LanguageDetector) // Tự động nhận diện ngôn ngữ máy tính khách
