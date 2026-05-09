548d7d0f4100300b6450bc205f52a32176e7bbb9
HuynhGiaBao <gia797978@gmail.com>
2026-05-09 15:56:53 +0700

    update

---
+++ backend/src/main/java/com/BBTT/BBTTResort/service/interfac/IBookingService.java
@@ -0,0 +1,22 @@
+package com.BBTT.BBTTResort.service.interfac;
+
+import com.BBTT.BBTTResort.dto.Response;
+import com.BBTT.BBTTResort.entity.Booking;
+
+public interface IBookingService {
+
+    Response saveBooking(Long roomId, Long userId, Booking bookingRequest);
+
+    Response findBookingByConfirmationCode(String confirmationCode);
+
+    Response getAllBookings();
+
+    Response getBookingsByUserId(Long userId);
+
+    Response cancelBooking(Long bookingId);
+
+    Response getBookingServicesByBookingId(Long bookingId);
+
+
+
+}
