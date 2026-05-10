5395d9655c2bb6ef9dc1f1527e1309a1591296aa
HungBuiHuu <hungbui12345h@gmail.com>
2026-05-10 12:03:09 +0700

    update

--- frontend/src/component/common/Footer.jsx
+++ frontend/src/component/common/Footer.jsx
@@ -13,16 +13,3 @@ const FooterComponent = () => {
 };
 
 export default FooterComponent;
-const FooterComponent = () => {
-
-
-    return (
-        <footer>
-            <span className="my-footer">
-                BBHH Resort | All Right Reserved &copy; {new Date().getFullYear()}
-            </span>
-        </footer>
-    );
-};
-
-export default FooterComponent;
